5cc310c947582b67c47eac27a93dfb81e02ae697
Aleksander Dimitrijevic <dimitrijevicaleksander.dev@gmail.com>
2020-01-29 15:02:13 +0100

    added register route

--- app.js
+++ app.js
@@ -1,23 +1,23 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
+const userRegister = require("./routes/users");
+app.use(express.static("public"));
+app.use(express.urlencoded());
+app.use(express.json());
 
-app.use(express.static('public'))
+app.use("/register", userRegister);
 
+app.get("/api/products", (req, res) => {
+  res.json([
+    {
+      _id: 1,
+      title: "Awesome shit",
+      price: 1337,
+      shortDesc: "lorem ipsum",
+      longDesc: "loredm dfsfsafmlfds",
+      imgFile: "skateboard-greta.png"
+    }
+  ]);
+});
 
-app.get('/api/products', (req,res) => {
-    res.json(
-        [
-            {
-                _id: 1,
-                title: "Awesome shit",
-                price: 1337,
-                shortDesc: "lorem ipsum",
-                longDesc: "loredm dfsfsafmlfds",
-                imgFile: 'skateboard-greta.png'
-            }
-        ]
-    )
-})
-
-
-app.listen(8080, () => console.log("Server started"))
+app.listen(8080, () => console.log("Server started"));
--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,19 @@
 const { Router } = require("express");
 const router = new Router();
 const bcrypt = require("bcryptjs");
+const User = require("../models/User");
 
-router.post("/api/register", (req, res) => {});
+router.post("/api/register", async (req, res) => {
+  const newUser = {
+    email: req.body.email,
+    password: await bcrypt.hash(req.body.password),
+    name: req.body.name,
+    street: req.body.street,
+    zip: req.body.zip,
+    city: req.body.city
+  };
+  const user = await User.register(newUser);
+  res.json(user);
+});
 
 module.exports = router;
